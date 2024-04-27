c6f96f3f0911a30b6c5d53d56ca35fff18448cc9
JinCheng <cs2338139@gmail.com>
2024-04-27 22:12:36 +0800

    feat: typography,color settting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+
+import Loading from './views/Loading';
+
+gsap.registerPlugin(ScrollTrigger,ScrollToPlugin,EaselPlugin);
+
 
 function App() {
 
+  const [isaLoading, setIsaLoading] = useState(false)
+
   return (
     <>
-
+      <Loading />
     </>
   )
 }
---
+++ src/views/Loading.jsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+    return(
+        <div className="h-screen w-screen flex justify-center flex-col items-center bg-black ">
+        {/* <div className="flex flex-col dev-1 sm:w-[60%] sm:h-[12%] justify-between items-center">
+          <div className="text-heading-2 text-custom-Primary-0 text-[50px]">VIBE 400</div>
+          <div className="w-full h-0.5 bg-custom-GrayScale-1">
+            <div className="w-0 h-0.5 bg-custom-Primary-0"></div>
+          </div>
+        </div> */}
+      </div>
+    )
+}
